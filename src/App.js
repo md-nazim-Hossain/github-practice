c8ec3a0853446dd82d0a94c9dd99da939b87af3a
md-nazim-Hossain <nazimhossaindpi@gmail.com>
2022-03-05 20:57:08 +0600

    added dev

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <h1>This is Development branch</h1>
       <p>Dev branch 2</p>
       <span>This Git DEv Branch</span>
+      <h2>This dev branch commit in dev</h2>
     </div>
   );
 }
